27040380ef1f8750820c223de63804cc21b63c01
Shokhrukh Nigmatillaev <shoxrux020902@gmail.com>
2024-09-15 21:28:28 +0200

    Add frames

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ window.minsize(width=500, height=300)
 # window.maxsize(width=1000, height=600)
 window.config(bg="yellow")
 window.geometry("300x300+300+300")
+left_frame = Frame(window, width=200, height=200, bg='grey')
+left_frame.grid(row=0, column=0, padx=10, pady=5)
+
+right_frame = Frame(window, width=325, height=200, bg='grey')
+right_frame.grid(row=0, column=1, padx=10, pady=5)
 
 # Main label creation
-mainLabel = Label(text="Type or loose it", font=("Courier", 20, "bold"), bg="yellow", fg="red")
-mainLabel.grid(padx=125, pady=10)
+mainLabel = Label(left_frame, text="Type or loose it", font=("Courier", 10, "bold"), fg="red",)
+mainLabel.grid(padx=5, pady=5)
 
 # Text entry
-input = Entry(width=50)
-input.grid()
+input = Entry(right_frame, width=50)
+input.grid(padx=5, pady=5)
 window.mainloop()
